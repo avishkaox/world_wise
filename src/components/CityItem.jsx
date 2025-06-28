686a3cfdbccad9a3c21ed84827f5859345f570a6
DESKTOP-7PHAR17\avish <avishka.madushanka123@gmail.com>
2025-06-28 09:15:32 +0530

    minor changes

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,10 +1,18 @@
 import styles from "./CityItem.module.css";
 
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
 function CityItem(props) {
   return (
     <li className={styles.cityItem}>
       <span className={styles.emoji}>{props.cities.emoji}</span>
       <h3 className={styles.name}>{props.cities.cityName}</h3>
+      <time className={styles.date}>{formatDate(props.cities.date)}</time>
     </li>
   );
 }
