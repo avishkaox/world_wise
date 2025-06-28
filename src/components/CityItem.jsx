e794110c8f067860b006dd80619aafcd652b033a
DESKTOP-7PHAR17\avish <avishka.madushanka123@gmail.com>
2025-06-28 09:30:52 +0530

    added new message component

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -13,6 +13,7 @@ function CityItem(props) {
       <span className={styles.emoji}>{props.cities.emoji}</span>
       <h3 className={styles.name}>{props.cities.cityName}</h3>
       <time className={styles.date}>{formatDate(props.cities.date)}</time>
+      <button className={styles.deleteBtn}>&times;</button>
     </li>
   );
 }
--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,12 +1,15 @@
 import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
-import Spinner from "./Spinner"
+import Spinner from "./Spinner";
+import Message from "./Message";
 
 function CityList(props) {
+  if (!props.cities.length) return <Message message="Add your first city by clicking on a city on the map" />;
+
   return (
     <>
       {props.isLoading ? (
-        <Spinner/>
+        <Spinner />
       ) : (
         <ul className={styles.cityList}>
           {props.cities.map((item, index) => (
