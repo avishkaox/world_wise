93b71c0e6df61376a8da6794c26946a36f25549c
DESKTOP-7PHAR17\avish <avishka.madushanka123@gmail.com>
2025-07-03 23:07:14 +0530

    added new component

---
+++ src/components/BackButton.jsx
@@ -0,0 +1,15 @@
+function BackButton() {
+  return (
+    <Button
+      type="back"
+      onClick={(e) => {
+        e.preventDefault();
+        navigate(-1);
+      }}
+    >
+      &larr; Back
+    </Button>
+  );
+}
+
+export default BackButton;
